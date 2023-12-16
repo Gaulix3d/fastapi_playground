2c5c81c644e7bc7c01fd1464fdfa35010d782c22
gaulix <vlnazarenko1199@gmail.com>
2023-12-16 22:02:33 +0100

    Added chat logics

--- chat/models.py
+++ chat/models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime, Integer
 from database import Base
 import datetime
 
 class MessageModel(Base):
     __tablename__ = "Messages"
+    message_id = Column(Integer, primary_key= True, index=True)
     email = Column(String)
     message = Column(String)
     sent_date = Column(DateTime)
